5579fb9c36a42f6ecedfc205415fee4b3eb7d575
Steven M. Van Belleghem <vanbelleghemsteven@hotmail.com>
2022-04-18 11:07:44 -0400

    Delete PAN_SV_chromatin_Genomics directory

--- PAN_SV_chromatin_Genomics/PAN_genome/linearlize_xmfa.sh
+++
@@ -1,1 +0,0 @@
-perl -pe 'chomp if /^[ATCGNSBDHVMRWYKatcgnsbdhvmrwyk-]/' SeqSeqPan_erato_melp_char.xmfa | sed 's/\=/\n\=/g' | sed 's/>/\n>/g' | sed '/^$/d'  > SeqSeqPan_erato_melp_char_noNewline.xmfa
